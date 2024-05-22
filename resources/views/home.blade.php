bf7342b6feb8f36d6485142a139418c48a5a6d6f
Guillermo Morales <morales.guillermo2503@gmail.com>
2024-05-22 00:48:36 -0600

    Implemented nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Gantt's Diagram</title>
 </head>
 <body>
+    <div class="topnav">
+        <a href="">About me</a>
+        <a href="">Gantt's Chart</a>
+        <a href="">App use</a>
+    </div>
+    <div>
+
+    </div>
+    <p>Hello this is my website</p>
 </body>
 </html>
